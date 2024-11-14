e763192e1b310cb7270b4b186fd24a88732bddc6
nquiros1987 <nquiros@cfia.or.cr>
2024-11-14 17:11:13 -0600

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Iniciar el servidor Voila en Render
-voila --port=$PORT --no-browser --enable_nbextensions=True --ip=0.0.0.0 Json_KML.ipynb
+voila --port=$PORT --no-browser Json_KML.ipynb
